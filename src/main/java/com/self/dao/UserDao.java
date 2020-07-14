9e3f84c5e02869ce16941d3c19e1144d7acddbd5
vera_glb <vera_glb@qq.com>
2020-07-14 18:49:32 +0800

    Initial commit首次使用git

---
+++ src/main/java/com/self/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.self.dao;
+
+import com.self.domain.User;
+
+import java.util.List;
+
+/**
+ * @Author Vera-D
+ * @Date 2020-07-14 18:43
+ */
+public interface UserDao {
+
+    List<User> findAll();
+}
---
+++ src/main/java/com/self/domain/User.java
@@ -0,0 +1,17 @@
+package com.self.domain;
+
+/**
+ * @Author Vera-D
+ * @Date 2020-07-14 18:42
+ */
+public class User {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
